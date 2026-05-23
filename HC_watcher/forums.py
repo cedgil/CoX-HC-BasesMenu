0e78befd5900230c616a4913c9175caee068e3e1
cedgil <876710+cedgil@users.noreply.github.com>
2026-05-23 15:51:22 +0200

    Create forums.py

---
+++ HC_watcher/forums.py
@@ -0,0 +1,10 @@
+FORUMS = [
+    {
+        "name": "Base Construction",
+        "url": "https://forums.homecomingservers.com/forum/30-base-construction/",
+    },
+    {
+        "name": "Events & Contests",
+        "url": "https://forums.homecomingservers.com/forum/102-events-contests/",
+    },
+]
