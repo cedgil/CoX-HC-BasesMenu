cd8f73be8fc890fdef39110ddb10ec48078e62fd
cedgil <876710+cedgil@users.noreply.github.com>
2026-05-23 14:16:07 +0200

    Create scraped_bases_sources.py

---
+++ scraped_bases_sources.py
@@ -0,0 +1,54 @@
+# =========================================================
+# SOURCES
+# =========================================================
+
+SOURCES = [
+
+    {
+        "url": "https://forums.homecomingservers.com/topic/62785-list-your-base-for-the-noncompetitive-our-based-showcase/",
+        "event_name": "Noncompetitive Base Showcase",
+        "event_type": "showcase",
+        "fields": {
+            "supergroup_name": "Supergroup Name:",
+            "shard": "Shard/Server:",
+            "base_code": "Base Code:",
+            "category": "Category to list base in:"
+        }
+    },
+
+    {
+        "url": "https://forums.homecomingservers.com/topic/56486-2025-homecoming-base-contest-rules-entries-thread/",
+        "event_name": "2025 Base Contest",
+        "event_type": "contest",
+        "fields": {
+            "supergroup_name": "Your base’s name:",
+            "shard": "The shard it is located on:",
+            "base_code": "The passcode for entry:",
+            "category": "The category your base is entering under:"
+        }
+    },
+
+    {
+        "url": "https://forums.homecomingservers.com/topic/57844-community-challenge-contest-pride-through-the-ages/",
+        "event_name": "2025 Pride through the age",
+        "event_type": "contest",
+        "fields": {
+            "supergroup_name": "Base or SG Name:",
+            "shard": "Shard:",
+            "base_code": "Passcode:"
+        }
+    },
+
+    {
+        "url": "https://forums.homecomingservers.com/topic/39881-2023-homecoming-base-contest-rules-entries-thread/",
+        "event_name": "2023 Base Contest",
+        "event_type": "contest",
+        "fields": {
+            "supergroup_name": "Base or SG Name:",
+            "shard": "Shard:",
+            "base_code": "Passcode:",
+            "category": "Category for Contest:"
+        }
+    }
+
+]
